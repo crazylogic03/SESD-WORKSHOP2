8f136f21791491e26fc7fed4b6b9f1f3d663ce1e
crazylogic03 <saithrishul2006@gmail.com>
2026-03-04 21:17:04 +0530

    feat: register 10 new commands in the CLI Engine Registry

--- cli.ts
+++ cli.ts
@@ -1,10 +1,33 @@
 #!/usr/bin/env node
 const CLI_Engine = require('./cliEngine/cli_engine');
-const AddCommand = require('./commands/addCommand')
-const GreetCommand = require('./commands/greetCommand')
-const JokeCommand = require('./commands/jokeCommand')
+const AddCommand = require('./commands/addCommand');
+const GreetCommand = require('./commands/greetCommand');
+const JokeCommand = require('./commands/jokeCommand');
+const SubtractCommand = require('./commands/subtractCommand');
+const MultiplyCommand = require('./commands/multiplyCommand');
+const DivideCommand = require('./commands/divideCommand');
+const RandomCommand = require('./commands/randomCommand');
+const TimeCommand = require('./commands/timeCommand');
+const UppercaseCommand = require('./commands/uppercaseCommand');
+const QuoteCommand = require('./commands/quoteCommand');
+const AdviceCommand = require('./commands/adviceCommand');
+const CatFactCommand = require('./commands/catFactCommand');
+const IpCommand = require('./commands/ipCommand');
 
 const engine = new CLI_Engine();
-engine.registerCommands([AddCommand, GreetCommand, JokeCommand]);
+engine.registerCommands([
+    AddCommand,
+    GreetCommand,
+    JokeCommand,
+    SubtractCommand,
+    MultiplyCommand,
+    DivideCommand,
+    RandomCommand,
+    TimeCommand,
+    UppercaseCommand,
+    QuoteCommand,
+    AdviceCommand,
+    CatFactCommand,
+    IpCommand
+]);
 engine.run();
-
